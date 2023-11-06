0f3ab30095de105534230848086e4335dd8930b5
Евдокимов Виталий <114080800+FoNoMaster@users.noreply.github.com>
2023-11-06 23:26:04 +0300

    Update main.py

--- mpl/Эпизод 1/main.py
+++ mpl/Эпизод 1/main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
+import os
 
-f = open('dead_moroz\\004.dat')
+f = open(os.path.join("dead_moroz", "005.dat"))
 a = []
 N = -1
 i = 0
@@ -16,7 +17,13 @@ for i in range(N):
     x.append(a[i][0])
     y.append(a[i][1])
 
-plt.scatter(x, y, s=5)
+plt.figure(figsize=((max(x) - min(x))/100, (max(y) - min(y))/100))
+plt.locator_params(axis='x', nbins=(max(x) - min(x))/50)
+plt.locator_params(axis='y', nbins=(max(y) - min(y))/50)
+
+plt.scatter(x, y, s=3)
 plt.title('Number of points: ' + str(N))
-plt.savefig("4.png")
+plt.savefig("5.png")
+
+
 plt.show()
