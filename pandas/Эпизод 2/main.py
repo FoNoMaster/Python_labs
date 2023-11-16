5021959cca24b8c084c2fc40daef9049184698e8
Евдокимов Виталий <114080800+FoNoMaster@users.noreply.github.com>
2023-11-16 10:33:08 +0300

    Add files via upload

--- pandas/Эпизод 2/main.py
+++ pandas/Эпизод 2/main.py
@@ -2,40 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("flights.csv")
+df.loc[:, 'NUMBER OF FLIGHTS'] = 1
 print(df)
-cargos = []
-for i in range(len(df)):
-    if cargos.count(df.iloc[i, 1]) == 0:
-        cargos.append(df.iloc[i, 1])
-print(cargos)
 
-flytimes = [0 for i in range(len(cargos))]
+data = df.groupby('CARGO').sum()
+print(data)
 
-for i in range(len(df)):
-    for j in range(len(cargos)):
-        if df.iloc[i, 1] == cargos[j]:
-            flytimes[j] = flytimes[j] + 1
-print(flytimes)
+data[['PRICE', 'WEIGHT', 'NUMBER OF FLIGHTS']].plot(kind='bar', subplots=True, figsize=(10, 20))
 
-data1 = df.loc[:, ['CARGO', 'WEIGHT']].groupby('CARGO').sum()
-print(data1)
-
-data2 = df.loc[:, ['CARGO', 'PRICE']].groupby('CARGO').sum()
-print(data2)
-
-data3 = pd.DataFrame(flytimes, index=cargos)
-
-ax = data1.plot(kind='bar', figsize=(10, 10))
-ax.set_xlabel('COMPANY')
-ax.set_ylabel('WEIGHT')
-
-bx = data2.plot(kind='bar', figsize=(10, 10))
-bx.set_xlabel('COMPANY')
-bx.set_ylabel('PRICE')
-
-cx = data3.plot(kind='bar', figsize=(10, 10))
-cx.set_xlabel('COMPANY')
-cx.set_ylabel('NUMBER OF FLIGHTS')
-
-plt.savefig("NUMBER OF FLIGHTS.png")
+plt.savefig("statistics.png")
 plt.show()
