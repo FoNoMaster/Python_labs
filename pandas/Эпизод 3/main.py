07c9084466f8344499f0ed220f75e42144fd42d9
Евдокимов Виталий <114080800+FoNoMaster@users.noreply.github.com>
2023-11-16 11:15:23 +0300

    Add files via upload

--- pandas/Эпизод 3/main.py
+++ pandas/Эпизод 3/main.py
@@ -13,39 +13,22 @@ print("=====================================")
 data = pd.merge(st_info, kr, on='User')
 print(data)
 
-fuculty_group = [i for i in data.groupby('group_faculty').sum().index]
-out_group = [i for i in data.groupby('group_out').sum().index]
+data1 = data.groupby(['group_faculty']).sum().loc[:, 'Solved']
+data2 = data.groupby('group_out').sum().loc[:, 'Solved']
 
-fuculty_average = []
-for i in fuculty_group:
-    fuculty_average.append(data.loc[data['group_faculty'] == i, 'Solved'].mean())
-
-group_average = []
-for i in out_group:
-    group_average.append(data.loc[data['group_out'] == i, 'Solved'].mean())
-
-fig, ax = plt.subplots(nrows=2)
+fig, axes = plt.subplots(2)
+fig.set_figwidth(9)
+fig.set_figheight(12)
 plt.subplots_adjust(hspace=0.5)
 
-
-ax[0].bar([str(i) for i in fuculty_group], fuculty_average)
-ax[1].bar([str(i) for i in out_group], group_average)
-ax[0].set_title('Solved tasks per fuculty group')
-ax[1].set_title('Solved tasks per inf group')
+data1.plot(kind='bar', ax=axes[0], title='Average solved tasks per fuculty group')
+data2.plot(kind='bar', ax=axes[1], title='Average solved tasks per inf group')
 plt.savefig("statistics.png")
 
 plt.show()
 
 print("======================================")
-fuc = []
-inf = []
 best = data[(data['H'] > 9) | (data['G'] > 9)].loc[:, ['group_faculty', 'group_out']]
-for i in range(len(best)):
-    if fuc.count(best.iloc[i, 0]) == 0:
-        fuc.append(best.iloc[i, 0])
-    if inf.count(best.iloc[i, 1]) == 0:
-        inf.append(best.iloc[i, 1])
 print("The best students in this groups:")
-print(fuc)
-print(inf)
-
+print(best.group_faculty.unique())
+print(best.group_out.unique())
