fc9e88061a5165f3072e6c7b35af567bcc2d8b2d
Евдокимов Виталий <114080800+FoNoMaster@users.noreply.github.com>
2023-12-02 12:43:27 +0300

    Add files via upload

--- SymPy & SciPy/Эпизод 2/main.py
+++ SymPy & SciPy/Эпизод 2/main.py
@@ -4,11 +4,9 @@ import matplotlib.pyplot as plt
 
 f = open("large.txt")
 N = int(f.readline())
-a = np.array([np.ones(N)], dtype=np.float64)
-for line in f:
-    a = np.append(a, [[float(j) for j in line.split()]], 0)
+a = np.loadtxt(f, dtype=np.float64)
 b = np.array(a[-1], dtype=np.float64)
-a = np.delete(a, [0, -1], 0)
+a = np.delete(a, [-1], 0)
 
 x = linalg.solve(a, b)
 
